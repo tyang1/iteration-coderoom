04b6ac53196e21c65ff90f83f51f1fe267d83ae8
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-17 21:43:12 -0700

    Continued creating Inventory Class and created InventoryItems Class in order to modularize.

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
-import './Inventory.css';
+// import './Inventory.css';
+
+/*
+Creating a box where the inventory lives.
+Need a variable "InventoryItems" which will pull the items from 'InventoryItems.js'
+
+CSS:
+Off white box.
+Long thin box that appears at the bottom of the screen.
+Box shadow.
+
+*/
 
 class Inventory extends Component {
     render() {
         return (
             <div className="inventory">
                 <h2>Inventory:</h2>
-                <p>Number of keys: </p>
+                {InventoryItems}
             </div>
         );
     }
