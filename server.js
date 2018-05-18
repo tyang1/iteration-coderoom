b6e8b1e9fa68478043ffaeac153349a13cd20c14
Mystique <helenk530@yahoo.com>
2018-05-18 14:27:33 -0700

    create css, add divs to boss.html, and add express.static to server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = 8080;
+
+
+app.use(express.static(path.join(__dirname, 'src')));
 
 
 app.get('/', (req, res) => {res.sendFile(__dirname + '/public/splash.html')});
@@ -13,7 +17,6 @@ app.get('/boss', (req, res) => {res.sendFile(__dirname + '/public/boss.html')});
 app.get('/win', (req, res) => {res.sendFile(__dirname + '/public/win.html')});
 
 app.get('/lose', (req, res) => {res.sendFile(__dirname + '/public/lose.html')});
-
 
 
 app.listen(PORT, () => console.log(`Listening to PORT: ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Interactive from "./Interactive";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <div className="narrative"></div>
+        <div className="interactive"></div>
+        <div className="inventory">Inventory</div>
+        {/* <Interactive /> */}
       </div>
     );
   }
