9241df5850cb8dfee7894ea43e091e9244ba2309
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-18 15:12:39 -0700

    added narrative text that will appear upon interaction

--- src/DungeonMaster.js
+++ src/DungeonMaster.js
@@ -9,7 +9,15 @@ class DungeonMaster extends Component {
     super(props)
     this.state = {
       keysCollected: 0,
-      text: 'text',
+      text: {
+          introText: "You wake up to find yourself in a dimly lit room. Wondering where you are you start to explore your small surroundings...",
+          deskText: "You head to the desk and search amongst the scattered sheets of paper: ",
+          nightstandText: "Wondering what might be inside you open the nightstand drawer: ",
+          bedText: "Hoping no monsters attack, you cautiously peek under the bed: ",
+          completionText: "Congratulations on completion of the challenge. You received 28 keys!",
+          bossChallengeText: "You have collected all the keys to the keyboard. Time for the Boss battle!",
+          bossDefeatText: "CONGRATULATIONS!!! You have succesfully defeated the Boss Challenge and ESCAPED!!!"
+      },
       promptText: '',
       buttons: {
         button1: {active: false, text: 'Check desk'},
