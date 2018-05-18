f3fc28a6d1bcf99050208fa1c9250029f7364a36
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-05-18 15:39:44 -0700

    adding more button functionality

--- src/DungeonMaster.js
+++ src/DungeonMaster.js
@@ -14,26 +14,31 @@ class DungeonMaster extends Component {
       button1: {active: false, text: 'Check desk'},
       button2: {active: false, text: 'Open nightstand drawer!'},
       button3: {active: false, text: 'Look under bed'},
-      button4: {visible: false, text: 'Challenge the boss'}
+      button4: {visible: false, text: 'Challenge the boss'},
       goToDesk: function() {
         // here we add the relevant narrative text to the active narrative array
         activeNarrative.push()
         // set button1 active so it's greyed out
         this.setState({button1: {active: true}});
+      },
+      goToNightstand: function() {
+        // here we add the relevant narrative text to the active narrative array
+        activeNarrative.push()
+        // set button1 active so it's greyed out
+        this.setState({button2: {active: true}});
+      },
+      goToBed: function() {
+        // here we add the relevant narrative text to the active narrative array
+        activeNarrative.push()
+        // set button1 active so it's greyed out
+        this.setState({button3: {active: true}});
       }
     }
     this.state.goToDesk = this.state.goToDesk.bind(this);
+    this.state.goToBed = this.state.goToBed.bind(this);
+    this.state.goToNightstand = this.state.goToNightstand.bind(this);
     // bind in-state functions here
 }
-
-goToDesk = () => {
-  this.setState({ button1: {active: true}});
-
-}
-
-openNightStandDrawer
-
-lookUnderBed
 
 render() {
     return (
@@ -41,7 +46,7 @@ render() {
             {this.props.children}
         </DM.Provider>
     );
-}
+  }
 }
 
 export { DungeonMaster,  DM };
