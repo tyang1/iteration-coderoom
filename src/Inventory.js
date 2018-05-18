201d7be6ccbeeb9bb8b0d6800f871bdfd884ae42
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-17 20:56:02 -0700

    Setting up the Inventory Class

---
+++ src/Inventory.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './Inventory.css';
+
+class Inventory extends Component {
+    render() {
+        return (
+            <div className="inventory">
+                <h2>Inventory:</h2>
+                <p>Number of keys: </p>
+            </div>
+        );
+    }
+}
+
+export default Inventory;
