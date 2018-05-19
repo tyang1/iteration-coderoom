6bcdacaf43e2ecde9083ab54a4b1d9afa0424efa
Mystique <helenk530@yahoo.com>
2018-05-19 15:13:32 -0700

    delete route to unused html file

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.get('/', (req, res) => {res.sendFile(__dirname + '/public/splash.html')});
 
 app.get('/game', (req, res) => {res.sendFile(__dirname + '/public/index.html')});
 
-app.get('/boss', (req, res) => {res.sendFile(__dirname + '/public/boss.html')});
-
 app.get('/win', (req, res) => {res.sendFile(__dirname + '/public/win.html')});
 
 app.get('/lose', (req, res) => {res.sendFile(__dirname + '/public/lose.html')});
