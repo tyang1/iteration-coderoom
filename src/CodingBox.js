bdb6196a4c5a2b90178d57ce1f512a7102a2c283
James <james@2-bit-toys.com>
2018-05-18 17:24:48 -0700

    Removed duplicate import

--- src/CodingBox.js
+++ src/CodingBox.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import { DM } from './DungeonMaster';
 // import "./CodingBox.css";
-import { DM } from "./DungeonMaster";
 
 class CodingBox extends Component {
   render() {
