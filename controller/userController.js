82ed1b2eac13ba72e44a8acaa87ae122d6baf3ee
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-05-22 16:04:09 -0700

    bcryption

--- controller/userController.js
+++ controller/userController.js
@@ -1,63 +1,66 @@
 const User = require("./../model/database");
 const userController = {};
-const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const SALT_WORK_FACTOR = 10;
 
-
-//salt generation
-var genRandomString = function(length){
-  return crypto.randomBytes(Math.ceil(length/2))
-          .toString('hex') /** convert to hexadecimal format */
-          .slice(0,length);   /** return required number of characters */
-};
-//has password with sha512
-var sha512 = function(password, salt){
-  var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-  hash.update(password);
-  var value = hash.digest('hex');
-  return {
-      salt:salt,
-      passwordHash:value
-  };
-};
 
 userController.checkExisting = (req,res,next) => {
     if(req.body.username && req.body.password){
         User.count({username : req.body.username}, (err,results) => {
-            if(err){ return res.send(err)}
-            if(results !== 0) {return res.send("the username is taken.")};
+            if(err){ res.send(err)}
+            if(results !== 0) {res.send("the username is taken.")};
             next();
         })
     }else{
-        return res.send("The username or password cannot be empty.")
+       res.send("The username or password cannot be empty.")
     }
 }
 
 userController.createUser = (req, res, next) => {
+  let {name,password} = req.body;
+  let salt;
+  let hash;
   if (
     typeof req.body.username === "string" &&
     typeof req.body.password === "string"
   ) {
-    let salt = genRandomString(16);
-    let passwordData = sha512(req.body.password, salt);
-    User.create(
+    let promise = new Promise(((resolve,reject) => {
+      console.log("in the new promise");
+      bcrypt.genSalt(SALT_WORK_FACTOR, (err,saltt) => {
+        console.log("bcrypt genSalt");
+        if(err) { console.log("salt generation err", err); reject(err);};
+        salt = saltt;
+        bcrypt.hash(password, saltt, (err,hash) => {
+          if(err) { console.log("hash generation err", err); reject};
+          password = hash;
+          console.log("this is the hash!!!!", hash);
+          resolve();
+        });
+      });
+    })).then(() => {
+      console.log("Armannnnnn");
+      User.create(
       {
         username: req.body.username,
-        password: passwordData,
+        password: password,
+        salt: salt,
         level: 0,
         totalTime: 0,
         createdAt: Date.now()
       },
       (err, updated) => {
-          if(err){ console.log(err);
-            res.send(err);
-          }else{
-              console.log("created user!!!!")
-              res.send(updated);
-          }
-
+        if(err){ console.log(err);
+          res.send(err);
+        }else{
+          console.log("created user!!!!")
+          res.send(updated);
+        }
       }
-    );
-  }
+    )})
+    // .catch(err){
+    //   console.log("ERRRRRR", err)
+    // }
+      }
 };
 
 module.exports = userController;
